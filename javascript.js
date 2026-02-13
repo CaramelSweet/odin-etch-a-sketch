aa57c412244eae70c98678053a6af8ccf6b48d27
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-02-13 15:09:50 -0500

    Create grid and hover function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-alert("Hello World!")
+const container = document.getElementById("container");
+
+// Set grid size
+const gridSize = 16;
+
+// Create divs for grid
+for (let i = 1; i <= gridSize * gridSize; i++) {
+    const gridBox = document.createElement("div");
+    gridBox.setAttribute("class", "square");
+    container.appendChild(gridBox);
+}
+
+const containerSize = gridSize * 32 + "px";
+container.style.width = containerSize;
+
+// Change color on hover
+container.addEventListener("mouseover", (event) => {
+    if (event.target.hasAttribute("class", "square")) {
+        event.target.setAttribute("class", "square filled");
+    }
+});
