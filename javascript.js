8cca9d5e42b3ffc4473e95ce5cdf62a1fbe42f61
Caramel Sweet <44335015+CaramelSweet@users.noreply.github.com>
2026-02-16 16:59:33 -0500

    Impliment user grid size button

--- javascript.js
+++ javascript.js
@@ -1,21 +1,54 @@
-const container = document.getElementById("container");
-
-// Set grid size
-const gridSize = 16;
-
-// Create divs for grid
-for (let i = 1; i <= gridSize * gridSize; i++) {
-    const gridBox = document.createElement("div");
-    gridBox.setAttribute("class", "square");
-    container.appendChild(gridBox);
+// Create grid function
+function createGrid (gridSize) {
+    for (let i = 1; i <= gridSize * gridSize; i++) {
+        const gridBox = document.createElement("div");
+        const squareSize = 100 / gridSize + "%";
+        gridBox.classList.add("square");
+        gridBox.style.width = squareSize;
+        container.appendChild(gridBox);
+    }
 }
 
-const containerSize = gridSize * 32 + "px";
-container.style.width = containerSize;
+// Check if use input is valid
+function isValidInput (input) {
+    const num = Number(input)
+
+    if (!Number.isInteger(num))  {
+        return false;
+    }
+
+    if (num >= 1 && num <= 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Define variables for event listeners 
+const container = document.getElementById("container");
+const button = document.getElementById("gridSizeButton");
 
 // Change color on hover
 container.addEventListener("mouseover", (event) => {
     if (event.target.hasAttribute("class", "square")) {
-        event.target.setAttribute("class", "square filled");
+        event.target.classList.add("filled");
     }
 });
+
+// Default grid size
+createGrid(16);
+
+// Listen for button event
+button.addEventListener("click", () => {
+    container.replaceChildren();
+
+    let gridSize = prompt("Enter grid size bewteen 1 and 100:", 16);
+
+    if (isValidInput(gridSize)) {
+        createGrid(gridSize);
+    } else {
+        createGrid(16);
+    }
+});
+
+
